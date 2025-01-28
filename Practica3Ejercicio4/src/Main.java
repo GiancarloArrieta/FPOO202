bc86145c4add5095def83b461ac3db373dc2f974
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-28 15:07:22 -0600

    Práctica 3, ejercicio 4

---
+++ Practica3Ejercicio4/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String apellidoP, apellidoM, nombre;
+        System.out.println("Ingrese su primer o unico nombre: ");
+        nombre = scan.nextLine();
+        System.out.println("Ingrese su apellido paterno: ");
+        apellidoP = scan.nextLine();
+        System.out.println("Ingrese su apellido materno: ");
+        apellidoM = scan.nextLine();
+        int total = nombre.length()+apellidoP.length()+apellidoM.length();
+        System.out.println("Nombre completo en mayusculas: " + nombre.toUpperCase() + " " + apellidoP.toUpperCase() + " " +  apellidoM.toUpperCase());
+        System.out.println("Su nombre completo completo consta de " + total + " letras.");
+    }
+}
