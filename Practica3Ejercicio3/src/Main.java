7489eff1922f2db5596408d30bb1896950af5a28
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-28 14:56:22 -0600

    Práctica 3, ejercicio 3

---
+++ Practica3Ejercicio3/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        int total = 0;
+        System.out.println("Ingrese un numero entero positivo: ");
+        numero = scan.nextInt();
+        if(numero>0){
+            for(int i=0; i<=numero; i++){
+                total += i;
+            }
+            System.out.println("Suma de todos los enteros de 1 a " + numero + ": " + total);
+        }else{
+            System.out.println("ERROR: El numero ingresado no es positivo.");
+        }
+    }
+}
