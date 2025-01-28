078fb0871809c95885d8e58dbbdb0a84c3fe1889
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-28 15:19:18 -0600

    Práctica 3, ejercicio 6

---
+++ Practica3Ejercicio6/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String frase, fraseInvertida = "";
+        System.out.println("Ingrese una frase: ");
+        frase = scan.nextLine();
+        for(int i = frase.length()-1; i >= 0; i--){
+            fraseInvertida += frase.charAt(i);
+        }
+        System.out.println("Frase invertida: " + fraseInvertida);
+    }
+}
