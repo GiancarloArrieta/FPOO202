c6cc1018839dd9ef96793ff9bca6902be0f65e1e
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-28 14:23:45 -0600

    Práctica 3, ejercicio 1
    "Crea un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde."

---
+++ Practica3Ejercicio1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int horas;
+        float salario;
+        System.out.println("Ingrese la cantidad de horas trabajadas: ");
+        horas = scan.nextInt();
+        System.out.println("Ingrese su salario por hora: ");
+        salario = scan.nextFloat();
+        System.out.println("A usted le corresponde un pago de $" + horas*salario);
+    }
+}
