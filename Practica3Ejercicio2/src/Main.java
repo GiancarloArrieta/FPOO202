24e9b48968296192c52a3f6b012efc8827d4137c
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-28 15:14:33 -0600

    Modificación a ejercicio 2

--- Practica3Ejercicio2/src/Main.java
+++ Practica3Ejercicio2/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         apellidoP = scan.nextLine();
         System.out.println("Ingrese su apellido materno: ");
         apellidoM = scan.nextLine();
-        String nombreCompleto = nombre + apellidoP + apellidoM;
+        String nombreCompleto = nombre + " " + apellidoP + " " + apellidoM;
         System.out.println(nombreCompleto.toLowerCase());
         System.out.println(nombreCompleto.toUpperCase());
         System.out.println(nombre.substring(0,1).toUpperCase() + apellidoP.substring(0,1).toUpperCase() + apellidoM.substring(0,1).toUpperCase());
