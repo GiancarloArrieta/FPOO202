170a0379f3122aa1ad3a4e98406f77ee889847bd
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-31 19:22:36 -0600

    Practica 5, ejercicio 2

--- Practica5/src/Ejercicio2.java
+++ Practica5/src/Ejercicio2.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan =  new Scanner(System.in);
         int numero;
         System.out.println("Ingrese un numero entero positivo: ");
         numero = scan.nextInt();
         if(numero>0){
-            for(int i = 1; i<=numero; i++){
-                if(i%2 != 0){
-                    System.out.print(i + ", ");
-                }
+            for(int i = numero; i>=0; i--){
+                System.out.print(i + ", ");
             }
         }else{
             System.out.println("ERROR: El numero ingresado no es positivo");
