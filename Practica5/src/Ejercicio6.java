bfa6b20bda2018b9d4a743cea5ba92a85d52a1ca
GiancarloArrieta <123045650@upq.edu.mx>
2025-01-31 22:15:38 -0600

    Práctica 5, ejercicio 6

---
+++ Practica5/src/Ejercicio6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int saldo = 0;
+        String operacion = null;
+        boolean continuar = true;
+        while(continuar){
+            System.out.println("Ingrese una operacion:\nD: Depositar (D n)\nR: Retirar (R n)\nNo ingrese nada para salir del programa");
+            operacion = scan.nextLine();
+            if(operacion.isEmpty()){
+                break;
+            }
+            if(operacion.toLowerCase().charAt(0) == 'd'){
+                    saldo += Integer.parseInt(operacion.substring(2));
+                }
+            if(operacion.toLowerCase().charAt(0) == 'r'){
+                saldo -= Integer.parseInt(operacion.substring(2));
+            }
+        }
+        System.out.println("Su saldo final es de: $" + saldo);
+    }
+}
