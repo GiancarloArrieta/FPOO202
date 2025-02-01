5dff9bc4e70f906e9ad847dde0b588da077f1980
GiancarloArrieta <123045650@upq.edu.mx>
2025-02-01 13:40:16 -0600

    Corrección ejercicio 7, práctica 5

--- Practica5/src/Ejercicio7.java
+++ Practica5/src/Ejercicio7.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
-public class Ejercicio7
-{
+public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int base;
         System.out.println("Ingrese un número impar la base del árbol:");
         base = scan.nextInt();
         if(base%2 != 0){
-            int altura = (base/2)+1;
-            for(int i=0; i<altura; i++){
-                for(int j = 0; j<(altura-i - 1); j++){
+            int i = 0, altura = (base/2)+1;
+            while (i < altura) {
+                int j = 0;
+                while (j < (altura - i - 1)) {
                     System.out.print(" ");
+                    j++;
                 }
-                for(int k=0; k<(2*i + 1); k++){
+                int k = 0;
+                while (k < (2 * i + 1)) {
                     System.out.print("*");
+                    k++;
                 }
-                System.out.print("\n");
+                System.out.println();
+                i++;
             }
         }else{
             System.out.println("ERROR: El número ingresado no es impar");
